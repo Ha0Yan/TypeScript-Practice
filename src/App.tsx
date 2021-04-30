9982cb56153ea59cf4ab7deee8660cb614366a3d
hal.yan <hal.yan@tntsupermarket.com>
2021-04-30 15:33:26 -0700

    TypeScript practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import List from "./components/List";
+import AddToList from "./components/AddToList";
+
+export interface IState {
+  people: {
+    name: string;
+    age: number;
+    url: string;
+    note?: string;
+  }[];
+}
 
 function App() {
+  const [people, setPeople] = useState<IState["people"]>([
+    {
+      name: "Hohn Smith",
+      url: "https://i.redd.it/spo5q1n66gg11.jpg",
+      age: 36,
+      note: "aslkjdasjkdlkasljkdlasjdk",
+    },
+  ]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Typescript Practice with React</h1>
+      <List people={people}></List>
+      <AddToList people={people} setPeople={setPeople} />
     </div>
   );
 }
